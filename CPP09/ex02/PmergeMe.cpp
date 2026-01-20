6d7865568d653bf0805201c8df06e1b288ce7deb
Rana Hosni <181972363+rana-hosni@users.noreply.github.com>
2026-01-20 17:05:13 +0100

    small fixes

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -40,16 +40,27 @@ std::vector<int> sortVector(std::vector<int> &vNumbers, int &comparisons){
         return vNumbers;
     }
     std::vector< std::pair<int, int> > pairs;
+
     for (size_t i = 0; i + 1 < vNumbers.size(); i += 2) {
-        // Keep pairs as vectors by wrapping them in groups of one element
         if (vNumbers[i] < vNumbers[i + 1]) {
             pairs.push_back(std::make_pair(vNumbers[i], vNumbers[i + 1]));
-        } else {
-            pairs.push_back(std::make_pair(vNumbers[i + 1], vNumbers[i]));
+            comparisons++;
 
+        } else {
+            comparisons++;
+            pairs.push_back(std::make_pair(vNumbers[i + 1], vNumbers[i]));
         }
-        comparisons++;
     }
+
+    //DEBUGGING
+    std::cout << "-----------------------------------------------------------" << std::endl;
+    std::cout << "Pairs formed (small, big): ";
+    for (size_t i = 0; i < pairs.size(); i++) {
+        std::cout << "(" << pairs[i].first << ", " << pairs[i].second << ") ";
+        // std::cout << " at index " << pairStructs[i].index << "; ";
+    }
+    std::cout << std::endl;
+    std::cout << "-----------------------------------------------------------" << std::endl;
 
     int leftover = -1;
     if (vNumbers.size() % 2 != 0) {
@@ -58,104 +69,125 @@ std::vector<int> sortVector(std::vector<int> &vNumbers, int &comparisons){
 
     std::vector<int> bigNumbers;
 
-    // std::cout << "Big numbers before sorting: ";
+    std::cout << "Big numbers before sorting: ";
     for (size_t i = 0; i < pairs.size(); i++) {
-        // std::cout << pairs[i].second << " ";
+        std::cout << pairs[i].second << " ";
         bigNumbers.push_back(pairs[i].second);
     }
 
-    // std::cout << std::endl;
+    std::cout << std::endl;
     std::vector<int> mainChain = sortVector(bigNumbers, comparisons);
 
     std::vector<int> smallNumbers;
-    for (size_t i = 0; i < pairs.size(); i++) {
-        smallNumbers.push_back(pairs[i].first);
+
+    
+    for (size_t i = 0 ; i < mainChain.size(); ++i) {
+        for (size_t j = 0; j < pairs.size(); ++j) {
+            if (mainChain[i] == pairs[j].second) {
+                smallNumbers.push_back(pairs[j].first);
+                break;
+            }
+        }
     }
+    for (size_t i = 0; i < pairs.size(); i++) {
+        std::cout << "pairs are (" << pairs[i].first << ", " << pairs[i].second << ")" << std::endl;
+    }
+
+    for (size_t i = 0; i < smallNumbers.size(); i++) {
+        std::cout << "Small number matched to main chain " << mainChain[i] << " is " << smallNumbers[i] << std::endl;
+    }
+    std::cout << " main chain   : ";
+    for (size_t i = 0; i < mainChain.size(); i++) {
+        std::cout << mainChain[i] << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << " small numbers: ";
+    for (size_t i = 0; i < smallNumbers.size(); i++) {
+        std::cout << smallNumbers[i] << " ";
+    }
+    std::cout << std::endl;
+
+
+    std::vector<int> order = jacobsthalOrder(smallNumbers.size());
+    int index;
+    int lookupIndex;
+    for (size_t i = 0; i < order.size(); i++) {
+        // std::cout << "Inserting small number " << smallNumbers[order[i]] << "in index " << index << " into main chain." << std::endl;
+        for (size_t k = 0 ; k < pairs.size(); k++) {
+            if (smallNumbers[order[i]] == pairs[k].first) {
+                lookupIndex = pairs[k].second;
+                break;
+            }
+        }
+        for (int j = 0; j < static_cast<int>(mainChain.size()); j++) {
+            if (mainChain[j] == lookupIndex) {
+                index = j;
+                break;
+            }
+        }
+        std::cout << "Inserting small number " << smallNumbers[order[i]] << " before index " << index << " (which has value " << mainChain[index] << ") into main chain." << std::endl;
+        mainChain = insertIntoMainChain(mainChain, smallNumbers[order[i]], index, comparisons);
+    }
+
     
     if (leftover != -1) {
-        smallNumbers.push_back(leftover);
+        mainChain = insertIntoMainChain(mainChain, leftover, mainChain.size(), comparisons);
     }
-    std::vector<int> order = jacobsthalOrder(smallNumbers);
-    insertIntoMainChain(mainChain, smallNumbers[0], 0, comparisons); // Insert the first small number directly
-    for (size_t i = 0; i < order.size(); i++) {
-        mainChain = insertIntoMainChain(mainChain, smallNumbers[order[i]], order[i], comparisons);
-    }
+
     return mainChain;
 }
 
-// J(n) = J(n-1) + 2*J(n-2)
+// // J(n) = J(n-1) + 2*J(n-2)
 
-std::vector<int> jacobsthalOrder(std::vector<int>& smallNumbers) {
-    int n = smallNumbers.size();
-    std::vector<int> order;
+std::vector<int> jacobsthalOrder(int n){
+    std::vector<int> order; 
     if (n == 0) 
         return order;
-    std::cout << "Small numbers for Jacobsthal order: ";
-    printContainer(smallNumbers);
-    std::vector<bool> used(n, false);
-
-    int j0 = 0; // J(0)
-    int j1 = 1; // J(1)
-    while (j1 < n) {
-        if (!used[j1]) {
-            order.push_back(j1);
-            used[j1] = true;
-            // std::cout << "j1 is" << j1 << " j0 is " << j0 << std::endl;
-        }
-        // std::cout << "Adding group between j1: " << j1 << " and j0: " << j0 << std::endl;
-        int group = j1 - 1;
-        while (group > j0) {
-            if (!used[group]) {
-                order.push_back(group);
-                used[group] = true;
-            }
-            group--;
-        }
-        int next = j1 + 2 * j0 ; // J(n) = J(n-1) + 2*J(n-2)
-        j0 = j1;
-        j1 = next;
+    // Jacobsthal numbers: 1, 3, 5, 11, ... 
+    int j0 = 1;
+    int j1 = 3; 
+    order.push_back(1); 
+    while (j1 <= n) { 
+        for (int i = j1; i > j0; --i) { 
+            order.push_back(i); 
+        } 
+        int next = j1 + 2 * j0; j0 = j1; j1 = next; 
     }
-    for (int i = n; i-- > j0;) {
-        std::cout << "Final adding of index: " << i << std::endl;
-        if (!used[i]) {
-            order.push_back(i);
-        }
+    // tail
+    for (int i = n; i > j0; --i) { 
+        order.push_back(i); 
     }
-    // std::cout << "Jacobsthal indices added:-------------------------TEST------------------------" << std::endl;
-    // printContainer(order);
-    // std::cout << "---------------------------------------------------------------" << std::endl;
-    
-    // for (int i = order.size(); i-- > 0;) {
-    //     order[i]--;
-    // }
-    std::cout << "Small numbers size : " << smallNumbers.size() << std::endl;
-    std::cout << "Jacobsthal order size : " << order.size() << std::endl;
-
-    std::cout << "Jacobsthal order : ";
+    // convert to 0-based
+    for (int i = 0; i <static_cast<int>(order.size()); ++i) 
+    { --order[i];
+    }
     printContainer(order);
-    return order;
+    return order; 
 }
 
-std::vector<int> insertIntoMainChain(std::vector<int> &mainChain, int value,int orderIndex ,int &comparisons){
-    size_t left = 0;
-    size_t right = mainChain.size();
-    // std::cout << "Inserting value " << value << " at order index " << orderIndex << std::endl;
-    if (orderIndex == 0) {
+
+// returns insertion position
+std::vector<int> insertIntoMainChain(std::vector<int> &mainChain, int value, size_t upperBoundExclusive, int &comparisons) {
+    if (upperBoundExclusive == 0) {
         mainChain.insert(mainChain.begin(), value);
         return mainChain;
     }
+    size_t left = 0;
+    size_t right = upperBoundExclusive; // search in [0, upperBoundExclusive)
     while (left < right) {
         size_t mid = left + (right - left) / 2;
+        comparisons++;
         if (mainChain[mid] < value) {
             left = mid + 1;
         } else {
             right = mid;
         }
-        comparisons++;
     }
     mainChain.insert(mainChain.begin() + left, value);
     return mainChain;
 }
+
 
 // DEQUE IMPLEMENTATION
 std::deque<int> sortDeque(std::deque<int> &dNumbers, int &comparisons){
@@ -163,6 +195,7 @@ std::deque<int> sortDeque(std::deque<int> &dNumbers, int &comparisons){
         return dNumbers;
     }
     std::deque< std::pair<int, int> > pairs;
+
     for (size_t i = 0; i + 1 < dNumbers.size(); i += 2) {
         // Keep pairs as vectors by wrapping them in groups of one element
         if (dNumbers[i] < dNumbers[i + 1]) {
@@ -248,3 +281,5 @@ std::deque<int> insertIntoMainChain(std::deque<int> &mainChain, int value, int &
     mainChain.insert(mainChain.begin() + left, value);
     return mainChain;
 }
+
+
--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -13,7 +13,6 @@
 
 
 
-
 template<typename T>
 void printContainer(const T &container) {
     for (typename T::const_iterator it = container.begin(); it != container.end(); ++it) {
@@ -22,24 +21,18 @@ void printContainer(const T &container) {
     std::cout << std::endl;
 }
 
-// Accept any callable (function pointer or function object) that takes T&
-// template<typename Func, typename T>
-// double calculateTime(Func sortFunction, T &container) {
-//     clock_t start = clock();
-//     sortFunction(container, 0); // assuming sortFunction takes container and an int reference for comparisons
-//     clock_t end = clock();
-//     return static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000000; // microseconds
-// }
 
 
 bool validateArguments(int argc, char **argv);
 void storeNumbers(const char** argv, std::vector<int> &vNumbers, std::deque<int> &dNumbers);
 
 std::vector<int> sortVector(std::vector<int> &vNumbers, int &comparisons);
+// std::vector<int> sortSmall(std::vector<int> &smallNumbers, std::vector<int> &mainChain, std::vector<std::pair<int, int>> &pairs);
 std::deque<int> sortDeque(std::deque<int> &dNumbers, int &comparisons);
-std::vector<int> insertIntoMainChain(std::vector<int> &mainChain, int value,int orderIndex ,int &comparisons);
+std::vector<int> insertIntoMainChain(std::vector<int> &mainChain, int value, size_t upperBoundExclusive, int &comparisons);
 std::deque<int> insertIntoMainChain(std::deque<int> &mainChain, int value, int &comparisons);
-std::vector<int> jacobsthalOrder(std::vector<int> &smallNumbers);
+std::vector<int> jacobsthalOrder(int n);
 std::deque<size_t> jacobsthalOrder(std::deque<int> &smallNumbers);
+
 
 #endif
